19c63f4258304cc93c38b510a180717f78521818
Proxfam <72892893+Proxfam@users.noreply.github.com>
2022-05-25 14:15:04 +1200

    25/05/22 #2

--- hopper_flutter_app/lib/pages/landingPage.dart
+++ hopper_flutter_app/lib/pages/landingPage.dart
@@ -17,29 +17,32 @@ class _landingPageState extends State<landingPage> {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
 
-    return SafeArea(
-        child: Scaffold(
-            body: SizedBox(
+    return Scaffold(
+        body: SafeArea(
+            child: SizedBox(
                 width: size.width,
                 height: size.height,
                 child: Column(
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        BorderBox(
-                          height: 50,
-                          width: 50,
-                          padding: EdgeInsets.all(8.0),
-                          child: Icon(Icons.menu, color: PRIMARY_COLOR),
-                        ),
-                        BorderBox(
-                            height: 50,
-                            width: 50,
-                            padding: EdgeInsets.all(8.0),
-                            child: Icon(Icons.settings, color: PRIMARY_COLOR))
-                      ],
-                    )
+                    Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: const [
+                            BorderBox(
+                              height: 50,
+                              width: 50,
+                              padding: EdgeInsets.all(8.0),
+                              child: Icon(Icons.menu, color: PRIMARY_COLOR),
+                            ),
+                            BorderBox(
+                                height: 50,
+                                width: 50,
+                                padding: EdgeInsets.all(8.0),
+                                child:
+                                    Icon(Icons.settings, color: PRIMARY_COLOR))
+                          ],
+                        ))
                   ],
                 ))));
   }
