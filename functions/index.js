3915c3ab6b40f8c089fd7b63e31008d0066f69a9
Proxfam <72892893+Proxfam@users.noreply.github.com>
2022-06-20 21:26:31 +1200

    20/06/22 3
    Big changes starting to work on the searching algorithm and I've introduced cloud functions for running high-level processing

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const serviceKey = require('./serviceKey.json');
 const functions = require('firebase-functions');
+const polyline = require('google-polyline');
 const cors = require('cors');
-const {Client, ReverseGeocodingLocationType, DirectionsResponseStatus} = require("@googlemaps/google-maps-services-js");
+const {Client, ReverseGeocodingLocationType, DirectionsResponseStatus, PlaceInputType} = require("@googlemaps/google-maps-services-js");
 const app = express();
 app.use(cors());
 const port = 3000;
@@ -17,7 +18,17 @@ initializeApp({
 
 const db = getFirestore();
 
+const GOOGLE_API_KEY = 'AIzaSyArRqDMu_RbWCN6PxHpfrZzGvN9D2wABfs';
+
 exports.addRiderToRide = async (req, res) => {
+    function _calculateDistance(lat1, lon1, lat2, lon2) {
+        var p = 0.017453292519943295;
+        var a = 0.5 -
+            Math.cos((lat2 - lat1) * p) / 2 +
+            Math.cos(lat1 * p) * Math.cos(lat2 * p) * (1 - Math.cos((lon2 - lon1) * p)) / 2;
+        return 12742 * Math.asin(Math.sqrt(a));
+    }
+
     var query = await db.collection('rides').get();
     var ride;
     var rideCoords;
@@ -26,23 +37,97 @@ exports.addRiderToRide = async (req, res) => {
             ride = query.docs[doc];
         }
     }
-    var query = await db.collection(`rides/${ride['id']}/coords`).get();
+
+    console.log(ride);
+
+    var query = await db.collection(`rides/${ride.id}/coords`).get();
     rideCoords = query.docs;
-    var response = await client.directions({
-        params: {
-            origin: '12 alexander place, arrowtown',
-            destination: '47 ferry hill drive, quail rise',
-            waypoints: ['arrowtown'],
-            mode: 'driving',
-            transitOptions: {
-                'departureTime': new Date().now
-            },
-            drivingOptions: {
-                'departureTime': new Date().now
-            },
-            key: 'AIzaSyBDPS7SYYqG6VlmkDe8hXmgmr4Jc0qm4bo'
-        },
-    })
+
+    var origin = await client.textSearch({params: {
+        query: req.query['origin'],
+        key: GOOGLE_API_KEY
+    }})
+
+    var destination = await client.textSearch({params: {
+        query: req.query['destination'],
+        key: GOOGLE_API_KEY
+    }})
+
+    var pickUp = {distance: 99999, coord: undefined};
+    var dropoff = {distance: 99999, coord: undefined};
+
+    for(coord in rideCoords) {
+        let result = _calculateDistance(
+            rideCoords[coord].data()['lat'],
+            rideCoords[coord].data()['lng'],
+            origin.data.results[0].geometry.location.lat,
+            origin.data.results[0].geometry.location.lng
+        );
+        if (result < pickUp.distance) {
+            pickUp.distance = result;
+            pickUp.coord = rideCoords[coord].data();
+        }
+    }
+
+    for(coord in rideCoords) {
+        let result = _calculateDistance(
+            rideCoords[coord].data()['lat'],
+            rideCoords[coord].data()['lng'],
+            destination.data.results[0].geometry.location.lat,
+            destination.data.results[0].geometry.location.lng
+        );
+        if (result < dropoff.distance) {
+            dropoff.distance = result;
+            dropoff.coord = rideCoords[coord].data();
+        }
+    }
+
+    console.log([pickUp,dropoff]);
+
+    //change this to work with closest point accessable by road using road matrix or smth
+    //It works but it doesn't take roads into account
+
+    res.send('done');
 };
 
+exports.addRide = async (req,res) => {
+    db.collection("rides")
+      .add({
+        'driver': '3n6ChQAe7qe25nl5Ju0zFiv6UWg1',
+        'distance': 'null',
+        'origin': [
+            -44.94889431082194, 
+            168.8449603099974
+        ],
+        'destination': [
+            -45.029701185507435, 
+            168.65722472525596
+        ],
+        'stops': []
+      })
+      .then(docRef => {
+        client.directions({
+            params: {
+                origin: {'lat':-44.94889431082194, 'lng':168.8449603099974},
+                destination: {'lat': -45.029701185507435, 'lng': 168.65722472525596},
+                waypoints: ['lower shotover road'],
+                key: GOOGLE_API_KEY
+            }
+        }).then(pointResult => {
+            let points = polyline.decode(pointResult.data.routes[0].overview_polyline.points);
+            for(point in points) {
+                db.collection(`rides/${docRef.id}/coords`).add({
+                    'lat': points[point][0],
+                    'lng': points[point][1]
+                })
+            }
+        });
+        // await FirebaseFirestore.instance
+        //   .collection('rides/${value.id}/coords')
+        //   .add({'lat': element.latitude, 'lng': element.longitude});
+    });
+    res.send();
+};
+
+exports.addRide = functions.https.onRequest(this.addRide);
 exports.addRiderToRide = functions.https.onRequest(this.addRiderToRide);
